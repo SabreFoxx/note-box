9648d4e127559607240c025bc18439f6748a3e1f
SabreFoxx <geraldnebs@gmail.com>
2023-03-16 18:17:04 +0100

    fix gitignore

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH="$(pwd)/dependencies/SFML-master/dist/lib"
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"$(pwd)/dependencies/SFML-master/dist/lib":"$(pwd)/dependencies/fruit-3.7.1/dist/lib"
 export LD_LIBRARY_PATH
 
 ./dist/notebox
